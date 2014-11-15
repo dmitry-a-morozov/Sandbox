a22497e7aa52b3e64afbbb79b03d733d19b6f1b6
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2014-11-14 19:21:48 -0800

    working with tuples output

--- SqlClientProvider/SqlClientProvider/SqlClientProvider.fs
+++ SqlClientProvider/SqlClientProvider/SqlClientProvider.fs
@@ -6,22 +6,25 @@ open System.Data
 open System.Data.SqlClient
 open System.Reflection
 open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Reflection
 
 open ProviderImplementation.ProvidedTypes
 
 [<TypeProvider>]
-type public SqlClientTypeProvider(config : TypeProviderConfig) as this = 
+type public SqlClientProvider(config : TypeProviderConfig) as this = 
     inherit TypeProviderForNamespaces()
 
     let nameSpace = this.GetType().Namespace
     let assembly = Assembly.GetExecutingAssembly()
-    let providerType = ProvidedTypeDefinition(assembly, nameSpace, "SqlClient", Some typeof<obj>)
+    let providerType = ProvidedTypeDefinition(assembly, nameSpace, "SqlClient", Some typeof<obj>, HideObjectMethods = true)
+    let sqlEngineTypeToClrMap = ref Map.empty
 
     do 
         providerType.DefineStaticParameters(
             parameters = [ 
                 ProvidedStaticParameter("CommandText", typeof<string>) 
                 ProvidedStaticParameter("ConnectionString", typeof<string>) 
+                ProvidedStaticParameter("SingleRow", typeof<bool>, false) 
             ],             
             instantiationFunction = this.CreateType
         )
@@ -30,41 +33,23 @@ type public SqlClientTypeProvider(config : TypeProviderConfig) as this =
     member internal this.CreateType typeName parameters = 
         let commandText : string = unbox parameters.[0] 
         let connectionString : string = unbox parameters.[1] 
+        let singleRow : bool = unbox parameters.[2] 
+
+        this.CheckMinimalVersion connectionString
+        this.LoadDataTypesMap connectionString
 
         let commandType = ProvidedTypeDefinition(assembly, nameSpace, typeName, baseType = Some typeof<obj>, HideObjectMethods = true)
 
         let parameters = this.ExtractParameters(connectionString, commandText)
-             
-        commandType.AddMembers [
-            yield this.Ctor(connectionString, commandText, parameters) :> MemberInfo
-            yield! this.GeneratePropsForParams parameters 
-            yield this.GenerateExecuteMethod(connectionString, commandText)
-        ]
 
-        commandType
-
-    member internal this.ExtractParameters(connectionString, commandText) : string list = 
-        [
-            use conn = new SqlConnection(connectionString)
-            conn.Open()
-            use cmd = new SqlCommand("sys.sp_describe_undeclared_parameters", conn, CommandType = CommandType.StoredProcedure)
-            cmd.Parameters.AddWithValue("@tsql", commandText) |> ignore
-            use reader = cmd.ExecuteReader()
-            while(reader.Read()) do
-                let paramName = string reader.["name"]
-                let dbTypeName = string reader.["suggested_system_type_name"]
-                let clrTypeName = reader.["suggested_system_type_id"] |> unbox |> this.MapSqlEngineTypeIdToClr 
-                yield sprintf "%s,%s,%s" paramName clrTypeName dbTypeName
-        ]
-
-    member internal this.Ctor(connectionString, commandText, parameters) = 
         ProvidedConstructor(
             parameters = [],
             InvokeCode = fun _ -> 
                 <@@ 
                     let this = new SqlCommand(commandText, new SqlConnection(connectionString)) 
                     for x in parameters do
-                        let paramName, sqlEngineTypeName = let xs = x.Split(',') in xs.[0], xs.[2]
+                        let xs = x.Split(',') 
+                        let paramName, sqlEngineTypeName = xs.[0], xs.[2]
                         let sqlEngineTypeNameWithoutSize = 
                             let openParentPos = sqlEngineTypeName.IndexOf('(')
                             if openParentPos = -1 then sqlEngineTypeName else sqlEngineTypeName.Substring(0, openParentPos)
@@ -73,87 +58,180 @@ type public SqlClientTypeProvider(config : TypeProviderConfig) as this =
                     this
                 @@>
         ) 
+        |> commandType.AddMember 
 
-    member internal this.GeneratePropsForParams parameters = 
-        
-        parameters
-        |> List.map (fun x -> 
-            let paramName, clrTypeName = let xs = x.Split(',') in xs.[0], xs.[1]
-            assert (paramName.StartsWith "@")
+        commandType.AddMembersDelayed <| fun() -> 
+            parameters
+            |> List.map (fun x -> 
+                let paramName, clrTypeName = let xs = x.Split(',') in xs.[0], xs.[1]
+                assert (paramName.StartsWith "@")
 
-            let prop = ProvidedProperty(propertyName = paramName.Substring 1, propertyType = Type.GetType clrTypeName)
-            prop.GetterCode <- fun args -> 
-                <@@ 
-                    let sqlCommand : SqlCommand = unbox %%args.[0]
-                    sqlCommand.Parameters.[paramName].Value
-                @@>
+                let prop = ProvidedProperty(propertyName = paramName.Substring 1, propertyType = Type.GetType clrTypeName)
+                prop.GetterCode <- fun args -> 
+                    <@@ 
+                        let sqlCommand : SqlCommand = %%Expr.Coerce(args.[0], typeof<SqlCommand>)
+                        sqlCommand.Parameters.[paramName].Value
+                    @@>
 
-            prop.SetterCode <- fun _ -> <@@ raise <| NotImplementedException() @@>
+                prop.SetterCode <- fun args -> 
+                    <@@ 
+                        let sqlCommand : SqlCommand = %%Expr.Coerce(args.[0], typeof<SqlCommand>)
+                        sqlCommand.Parameters.[paramName].Value <- %%Expr.Coerce(args.[1], typeof<obj>)
+                    @@>
 
-            prop.SetterCode <- fun args -> 
-                <@@ 
-                    let sqlCommand : SqlCommand = unbox %%args.[0]
-                    sqlCommand.Parameters.[paramName].Value <- %%Expr.Coerce(args.[1], typeof<obj>)
-                @@>
+                prop
+            )
 
-            upcast prop
-        )
+        use conn = new SqlConnection(connectionString)
+        use cmd = new SqlCommand("sys.sp_describe_first_result_set", conn, CommandType = CommandType.StoredProcedure)
+        cmd.Parameters.AddWithValue("@tsql", commandText) |> ignore
+        conn.Open()
+        use reader = cmd.ExecuteReader()
+        if not reader.HasRows
+        then 
+            this.AddExecuteNonQuery commandType
+        else
+            this.AddExecuteReader(reader, commandType, singleRow)
+        commandType
 
-    member internal this.GenerateExecuteMethod(connectionString, commandText) = 
-        let execute = ProvidedMethod("Execute", [], typeof<Async<seq<Map<string, obj>>>>)
-        execute.InvokeCode <- fun args -> 
-            <@@ 
+    member this.ExtractParameters(connectionString, commandText) : string list =  
+        [
+            use conn = new SqlConnection(connectionString)
+            conn.Open()
+
+            use cmd = new SqlCommand("sys.sp_describe_undeclared_parameters", conn, CommandType = CommandType.StoredProcedure)
+            cmd.Parameters.AddWithValue("@tsql", commandText) |> ignore
+            use reader = cmd.ExecuteReader()
+            while(reader.Read()) do
+                let paramName = string reader.["name"]
+                let clrTypeName = this.MapSqlEngineTypeToClr(sqlEngineTypeId = unbox<int> reader.["suggested_system_type_id"], detailedMessage = " Parameter name:" + paramName)
+                let dbTypeName = string reader.["suggested_system_type_name"]
+                yield sprintf "%s,%s,%s" paramName clrTypeName dbTypeName
+        ]
+
+    member __.CheckMinimalVersion connectionString = 
+        use conn = new SqlConnection(connectionString)
+        conn.Open()
+        let majorVersion = conn.ServerVersion.Split('.').[0]
+        if int majorVersion < 11 then failwithf "Minimal supported major version is 11. Currently used: %s" conn.ServerVersion
+
+    member __.LoadDataTypesMap connectionString = 
+        if sqlEngineTypeToClrMap.Value.IsEmpty
+        then
+            use conn = new SqlConnection(connectionString)
+            conn.Open()
+            sqlEngineTypeToClrMap := query {
+                let getSysTypes = new SqlCommand("SELECT * FROM sys.types", conn)
+                for x in conn.GetSchema("DataTypes").AsEnumerable() do
+                join y in (getSysTypes.ExecuteReader(CommandBehavior.CloseConnection) |> Seq.cast<IDataRecord>) on 
+                    (x.Field("TypeName") = string y.["name"])
+                let system_type_id = y.["system_type_id"] |> unbox<byte> |> int 
+                select(system_type_id, x.Field<string>("DataType"))
+            }
+            |> Map.ofSeq
+
+    member __.MapSqlEngineTypeToClr(sqlEngineTypeId, detailedMessage) = 
+        match !sqlEngineTypeToClrMap |> Map.tryFind sqlEngineTypeId with
+        | Some clrType ->  clrType
+        | None -> failwithf "Cannot map sql engine type %i to CLR type. %s" sqlEngineTypeId detailedMessage
+
+    member internal this.AddExecuteNonQuery commandType = 
+        let execute = ProvidedMethod("Execute", [], typeof<Async<unit>>)
+        execute.InvokeCode <- fun args ->
+            <@@
                 async {
-                    let sqlCommand : SqlCommand = %%Expr.Coerce(args.[0], typeof<SqlCommand>)
-                    sqlCommand.Connection.Open() 
-                    let! reader = sqlCommand.ExecuteReaderAsync() |> Async.AwaitTask
-                    return seq {
-                        while(reader.Read()) do
-                            yield seq {
-                                for i = 0 to reader.FieldCount - 1 do
-                                    yield reader.GetName i, reader.GetValue i
-                            } |> Map.ofSeq
-
-                        sqlCommand.Connection.Close()
-                    } |> Seq.cache
+                    let sqlCommand = %%Expr.Coerce(args.[0], typeof<SqlCommand>) : SqlCommand
+                    do! sqlCommand.Connection.OpenAsync()  |> Async.AwaitIAsyncResult |> Async.Ignore
+                    return! sqlCommand.ExecuteNonQueryAsync() |> Async.AwaitTask |> Async.Ignore 
                 }
             @@>
+        commandType.AddMember execute
 
-        upcast execute
+    static member internal GetRows<'Row>(cmd, rowMapper : Expr, singleRow) = 
+        let commandBehavior = if singleRow then CommandBehavior.SingleRow else CommandBehavior.Default 
+        //Combine with CommandBehavior.SingleResult for single result set ???
+        let getSeqAsyncCode = 
+            <@ 
+                async {
+                    let sqlCommand : SqlCommand = %%Expr.Coerce(cmd, typeof<SqlCommand>)
+                    let! token = Async.CancellationToken
+                    do! sqlCommand.Connection.OpenAsync(token) |> Async.AwaitIAsyncResult |> Async.Ignore
+                    let! reader = 
+                        try 
+                            sqlCommand.ExecuteReaderAsync(commandBehavior, token) |> Async.AwaitTask
+                        with _ ->
+                            sqlCommand.Connection.Close()
+                            reraise()
+                    return 
+                        seq {
+                            try 
+                                while(not token.IsCancellationRequested && reader.Read()) do
+                                    let row = Array.zeroCreate reader.FieldCount
+                                    reader.GetValues row |> ignore
+                                    yield (%%rowMapper : obj[] -> 'Row)  row 
+                            finally 
+                                sqlCommand.Connection.Close()
+                        } |> Seq.cache
+                }
+            @>
 
-    member internal this.MapSqlEngineTypeIdToClr(typeNameId : int) = 
-        let clrType = 
-            match typeNameId with
-            | 127 ->    typeof<int64>   //bigint
-            | 173 ->    typeof<byte[]>  //binary
-            | 104 ->    typeof<bool>    //bit
-            | 175 ->    typeof<string>  //char
-            | 40 ->     typeof<DateTime>  //date
-            | 61 ->     typeof<DateTime>  //datetime
-            | 42 ->     typeof<DateTime>  //datetime2
-            | 43 ->     typeof<DateTimeOffset>  //datetimeoffset
-            | 106 ->    typeof<decimal>  //decimal
-            | 62 ->     typeof<float>  //float
-            | 34 ->     typeof<byte[]>  //image
-            | 56 ->     typeof<int>  //int
-            | 60 ->     typeof<decimal>  //money
-            | 239 ->    typeof<string>  //nchar
-            | 99 ->     typeof<string>  //ntext
-            | 108 ->    typeof<int64>  //numeric
-            | 231 ->    typeof<string>  //nvarchar
-            | 59 ->     typeof<single>  //real
-            | 58 ->     typeof<DateTime>  //smalldatetime
-            | 52 ->     typeof<int16>  //smallint
-            | 122 ->    typeof<decimal>  //smallmoney
-            | 98 ->     typeof<obj>  //sql_variant
-            | 35 ->     typeof<string>  //text
-            | 41 ->     typeof<TimeSpan>  //time
-            | 48 ->     typeof<byte>  //tinyint
-            | 36 ->     typeof<Guid>  //uniqueidentifier
-            | 165 ->    typeof<byte[]>  //varbinary
-            | 167 ->    typeof<string>  //varchar
-            | _ -> failwithf "Unsupported sql engine type %i" typeNameId       
-        clrType.FullName
+        if singleRow
+        then 
+            <@@ 
+                async { 
+                    let! xs = %getSeqAsyncCode
+                    return Seq.exactlyOne xs
+                }
+            @@>
+        else
+            upcast getSeqAsyncCode
+
+    static member internal GetValues0<'Row>(cmd, singleRow) = 
+        SqlClientProvider.GetRows<'Row>(cmd, <@ fun (values : obj[]) -> unbox<'Row> values.[0] @>, singleRow)
+
+    member internal this.AddExecuteReader(columnInfoReader, commandType, singleRow) = 
+        let columns = 
+            columnInfoReader 
+            |> Seq.cast<IDataRecord> 
+            |> Seq.map (fun x -> 
+                let columnName = string x.["name"]
+                columnName, 
+                this.MapSqlEngineTypeToClr(sqlEngineTypeId = unbox x.["system_type_id"], detailedMessage = " Column name:" + columnName),
+                unbox<int> x.["column_ordinal"]
+            ) 
+            |> Seq.toList
+
+        if columns.Length = 1
+        then
+            let _, itemTypeName, _ = columns.Head
+
+            let itemType = Type.GetType itemTypeName
+            let returnType = 
+                let asyncSpecialization = if singleRow then itemType else typedefof<_ seq>.MakeGenericType itemType 
+                typedefof<_ Async>.MakeGenericType asyncSpecialization
+
+            let execute = ProvidedMethod("Execute", [], returnType)
+
+            execute.InvokeCode <- fun args -> 
+                let impl = this.GetType().GetMethod("GetValues0", BindingFlags.NonPublic ||| BindingFlags.Static).MakeGenericMethod([| itemType |])
+                impl.Invoke(null, [| args.[0]; singleRow |]) |> unbox
+            commandType.AddMember execute
+
+        else 
+            let syncReturnType, executeMethodBody = 
+                let tupleType = columns |> List.map (fun(_, typeName, _) -> Type.GetType typeName) |> List.toArray |> FSharpType.MakeTupleType
+                let rowMapper = 
+                    let values = Var("values", typeof<obj[]>)
+                    let getTupleType = Expr.Call(typeof<Type>.GetMethod("GetType", [| typeof<string>|]), [ Expr.Value tupleType.AssemblyQualifiedName ])
+                    Expr.Lambda(values, Expr.Coerce(Expr.Call(typeof<FSharpValue>.GetMethod("MakeTuple"), [Expr.Var values; getTupleType]), tupleType))
+                let getExecuteBody(args : Expr list) = 
+                    let impl = this.GetType().GetMethod("GetRows", BindingFlags.NonPublic ||| BindingFlags.Static).MakeGenericMethod([| tupleType |])
+                    impl.Invoke(null, [| args.[0]; rowMapper; singleRow |]) |> unbox
+
+                let resultType = if singleRow then tupleType else typedefof<_ seq>.MakeGenericType(tupleType)
+                resultType, getExecuteBody
+                    
+            commandType.AddMember <| ProvidedMethod("Execute", [], typedefof<_ Async>.MakeGenericType syncReturnType, InvokeCode = executeMethodBody)
 
 [<assembly:TypeProviderAssembly>]
 do()
--- SqlClientProvider/SqlClientProvider/Test.fsx
+++ SqlClientProvider/SqlClientProvider/Test.fsx
@@ -6,6 +6,9 @@ open FSharp.Data
 [<Literal>]
 let connection = "Data Source=(localdb)\ProjectsV12;Initial Catalog=master;Integrated Security=True"
 
-type GetDatabaseById = SqlClient<"SELECT name FROM sys.databases WHERE database_id = @id", connection>
+type GetDatabaseById = SqlClient<"SELECT database_id, name FROM sys.databases WHERE database_id = @id", connection>
 let getDatabaseById = GetDatabaseById(id = 1)
 getDatabaseById.Execute() |> Async.RunSynchronously |> Seq.toArray
+
+type GetDate = SqlClient<"SELECT GETDATE()", connection, SingleRow = true>
+GetDate().Execute() |> Async.RunSynchronously 
