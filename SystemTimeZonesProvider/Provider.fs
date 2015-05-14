eddb5d507a67dc949550df0c9c73a749fb75d8e8
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2015-05-13 21:33:24 -0700

    SystemTimeZonesProvider

---
+++ SystemTimeZonesProvider/Provider.fs
@@ -0,0 +1,32 @@
+﻿namespace System
+
+open System.Reflection
+open Microsoft.FSharp.Core.CompilerServices
+
+open ProviderImplementation.ProvidedTypes
+
+[<TypeProvider>]
+type public SystemTimeZonesProvider(config : TypeProviderConfig) as this = 
+    inherit TypeProviderForNamespaces()
+
+    let nameSpace = this.GetType().Namespace
+    let assembly = Assembly.LoadFrom( config.RuntimeAssembly)
+
+    let t = ProvidedTypeDefinition(assembly, nameSpace, "SystemTimeZones", Some typeof<obj>, HideObjectMethods = true)
+
+    do 
+        t.AddMembers [
+            for x in TimeZoneInfo.GetSystemTimeZones() do
+                let id = x.Id
+                yield ProvidedProperty(x.DisplayName, typeof<TimeZoneInfo>, [], IsStatic = true, GetterCode = fun _ -> <@@ TimeZoneInfo.FindSystemTimeZoneById id @@>)
+        ]
+
+        this.AddNamespace(nameSpace, [ t ])
+
+open System.Runtime.CompilerServices
+
+[<assembly:TypeProviderAssembly>]
+[<assembly:AssemblyVersion("0.1.0.0")>]
+do ()
+
+    
---
+++ SystemTimeZonesProvider/Test.fsx
@@ -0,0 +1,7 @@
+﻿#r @"bin\Debug\SystemTimeZonesProvider.dll"
+
+open System
+
+SystemTimeZones.``(UTC-08:00) Pacific Time (US & Canada)``
+ 
+SystemTimeZones.``(UTC) Dublin, Edinburgh, Lisbon, London``
