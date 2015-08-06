ac5f9f4e9bbe3c03b3563e79ab7427a10380cd6c
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2015-08-05 20:21:28 -0700

    initial check-in

---
+++ FSharp.Data.Entity/FSharp.Data.Entity.Tests/Basic.fs
@@ -0,0 +1,13 @@
+﻿module Basic
+
+open Xunit
+open FSharp.Data.Entity
+
+type AdventureWorks = DbContext<"Data Source=.;Initial Catalog=AdventureWorks2014;Integrated Security=True">
+
+let db = new AdventureWorks()
+
+[<Fact>]
+let getShifts() = 
+    ()
+
---
+++ FSharp.Data.Entity/FSharp.Data.Entity/DbContextProvider.fs
@@ -0,0 +1,53 @@
+﻿namespace FSharp.Data.Entity
+
+open System.Reflection
+open System.IO
+open Microsoft.Data.Entity
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+
+[<TypeProvider>]
+type public DbContextProvider(config: TypeProviderConfig) as this = 
+    inherit TypeProviderForNamespaces()
+
+    let nameSpace = this.GetType().Namespace
+    let assembly = Assembly.LoadFrom( config.RuntimeAssembly)
+    let providerType = ProvidedTypeDefinition(assembly, nameSpace, "DbContext", Some typeof<obj>, HideObjectMethods = true, IsErased = false)
+
+    let tempAssembly =  
+        let name = Path.ChangeExtension( Path.GetRandomFileName(), "dll")
+        let fullPath = Path.Combine(config.TemporaryFolder, name)
+        ProvidedAssembly fullPath
+
+    do tempAssembly.AddTypes [ providerType ]
+
+    do 
+        providerType.DefineStaticParameters(
+            parameters = [ 
+                ProvidedStaticParameter("ConnectionString", typeof<string>) 
+            ],             
+            instantiationFunction = (fun typeName args ->   
+                this.CreateRootType(typeName, unbox args.[0])
+            )        
+        )
+
+        this.AddNamespace( nameSpace, [ providerType ])
+    
+    member internal this.CreateRootType( typeName, connectionString) = 
+        let rootType = ProvidedTypeDefinition(assembly, nameSpace, typeName, baseType = Some typeof<DbContext>, HideObjectMethods = true, IsErased = false)
+        
+        let ctor = ProvidedConstructor([])
+        ctor.BaseConstructorCall <-
+            let baseCtor = typeof<DbContext>.GetConstructor(BindingFlags.NonPublic, null, [||], null)
+            fun _ -> baseCtor, []
+        rootType.AddMember ctor
+
+        rootType.AddMembersDelayed <| this.GetEntities
+
+        rootType
+
+    member internal this.GetEntities() = 
+        []
+
+[<assembly:TypeProviderAssembly()>]
+do()
