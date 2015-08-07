fdc76a1c705d6b6f8b67c548ab7c6099cd388537
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2015-08-06 19:27:10 -0700

    finally DbContext ctor works

--- FSharp.Data.Entity/FSharp.Data.Entity.Tests/Basic.fs
+++ FSharp.Data.Entity/FSharp.Data.Entity.Tests/Basic.fs
@@ -5,9 +5,10 @@ open FSharp.Data.Entity
 
 type AdventureWorks = DbContext<"Data Source=.;Initial Catalog=AdventureWorks2014;Integrated Security=True">
 
-let db = new AdventureWorks()
-
 [<Fact>]
 let getShifts() = 
+    let db = new AdventureWorks()
+    //let x = new AdventureWorks.``HumanResources.Shift``()
+    //printfn "%A" x
     ()
 
--- FSharp.Data.Entity/FSharp.Data.Entity/DbContextProvider.fs
+++ FSharp.Data.Entity/FSharp.Data.Entity/DbContextProvider.fs
@@ -1,9 +1,13 @@
 ﻿namespace FSharp.Data.Entity
 
+open System
 open System.Reflection
 open System.IO
+open System.Data
+open System.Data.SqlClient
 open Microsoft.Data.Entity
 open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Quotations
 open ProviderImplementation.ProvidedTypes
 
 [<TypeProvider>]
@@ -14,12 +18,24 @@ type public DbContextProvider(config: TypeProviderConfig) as this =
     let assembly = Assembly.LoadFrom( config.RuntimeAssembly)
     let providerType = ProvidedTypeDefinition(assembly, nameSpace, "DbContext", Some typeof<obj>, HideObjectMethods = true, IsErased = false)
 
-    let tempAssembly =  
+    let (?) (row: DataRow) (name: string) = row.Field name
+
+    let tempAssembly = 
         let name = Path.ChangeExtension( Path.GetRandomFileName(), "dll")
         let fullPath = Path.Combine(config.TemporaryFolder, name)
         ProvidedAssembly fullPath
 
-    do tempAssembly.AddTypes [ providerType ]
+    do 
+        tempAssembly.AddTypes [ providerType ]
+
+//    let addGeneratedTypes types =  
+//        let name = Path.ChangeExtension( Path.GetRandomFileName(), "dll")
+//        let fullPath = Path.Combine(config.TemporaryFolder, name)
+//        let tempAssembly = ProvidedAssembly fullPath
+//        tempAssembly.AddTypes types
+
+//    do 
+//        addGeneratedTypes [ providerType ]
 
     do 
         providerType.DefineStaticParameters(
@@ -33,21 +49,89 @@ type public DbContextProvider(config: TypeProviderConfig) as this =
 
         this.AddNamespace( nameSpace, [ providerType ])
     
+    override __.ResolveAssembly args =
+        let missing = AssemblyName(args.Name)
+
+        config.ReferencedAssemblies
+        |> Seq.tryPick(fun assemblyFile ->
+            let reference = Assembly.ReflectionOnlyLoadFrom( assemblyFile).GetName()
+            if AssemblyName.ReferenceMatchesDefinition(reference, missing)
+            then assemblyFile |> Assembly.LoadFrom |> Some
+            else None
+        )
+        |> defaultArg <| base.ResolveAssembly( args)
+
     member internal this.CreateRootType( typeName, connectionString) = 
         let rootType = ProvidedTypeDefinition(assembly, nameSpace, typeName, baseType = Some typeof<DbContext>, HideObjectMethods = true, IsErased = false)
-        
-        let ctor = ProvidedConstructor([])
-        ctor.BaseConstructorCall <-
-            let baseCtor = typeof<DbContext>.GetConstructor(BindingFlags.NonPublic, null, [||], null)
-            fun _ -> baseCtor, []
-        rootType.AddMember ctor
+        //let rootType = ProvidedTypeDefinition(assembly, nameSpace, typeName, baseType = None, HideObjectMethods = true, IsErased = false)
+        tempAssembly.AddTypes [ rootType ]
 
-        rootType.AddMembersDelayed <| this.GetEntities
+        do
+            let ctor = ProvidedConstructor([], IsImplicitCtor = true)
+            let baseCtor = typeof<DbContext>.GetConstructor(BindingFlags.Instance ||| BindingFlags.NonPublic, null, [||], null)
+            ctor.BaseConstructorCall <- fun args -> baseCtor, args
+            rootType.AddMember ctor
+
+//        rootType.AddMembersDelayed <| fun() ->
+//            let xs = this.GetEntities( connectionString)
+//            tempAssembly.AddTypes xs
+//            xs
 
         rootType
 
-    member internal this.GetEntities() = 
-        []
+    member internal this.GetEntities(connectionString: string) = [
+        use conn = new SqlConnection(connectionString)
+        conn.Open()
+
+        let types = 
+            dict [
+                for x in conn.GetSchema("DataTypes").Rows do
+                    let typeName = string x.["TypeName"]
+                    yield
+                        match typeName.Split([|','|], 2) with
+                        | [| "Microsoft.SqlServer.Types.SqlHierarchyId"; _ |] -> "hierarchyid", typeName
+                        | [| "Microsoft.SqlServer.Types.SqlGeometry"; _ |] -> "geometry", typeName
+                        | [| "Microsoft.SqlServer.Types.SqlGeography"; _ |] -> "geography", typeName
+                        | _ -> typeName, string x.["DataType"]
+            ] 
+
+        for row in conn.GetSchema("Tables", restrictionValues = [| null; null; null; "BASE TABLE" |]).Rows do   
+
+            let schema, name = row ? table_schema, row ? table_name
+            let tableType = ProvidedTypeDefinition(className = sprintf "%s.%s" schema name , baseType = None, IsErased = false)
+
+            do 
+                let ctor = ProvidedConstructor []
+                ctor.BaseConstructorCall <- fun _ -> typeof<obj>.GetConstructor([||]), []
+                //ctor.InvokeCode <- fun _ -> <@@ () @@>
+                ctor.IsImplicitCtor <- true
+                tableType.AddMember ctor
+
+            do 
+                tableType.AddMembersDelayed <| fun() -> 
+                    [
+                        use conn = new SqlConnection(connectionString)
+                        conn.Open()
+                        let columns = conn.GetSchema("Columns", restrictionValues = [| null; schema; name; null |]).Rows
+                        for c in columns do
+                            let colName = c ? column_name
+                            let dataType = 
+                                let typeName = c ? data_type
+                                Type.GetType(types.[typeName], throwOnError = true)
+                            let isNullable = c ? is_nullable
+
+                            let backingField = ProvidedField(colName, dataType)
+                            yield backingField :> MemberInfo
+                        
+                            let property = ProvidedProperty(colName, dataType)
+                            property.GetterCode <- fun args -> Expr.FieldGet( args.[0], backingField)
+                            yield upcast property
+                    ]
+
+            yield tableType
+    ]
+        
 
 [<assembly:TypeProviderAssembly()>]
 do()
+
