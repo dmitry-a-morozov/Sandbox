09376079a124382a509474bcc061552edb76d264
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2015-09-13 19:50:55 -0700

    .

--- NonSealedGenTypes/NonSealedGenTypes/Test.fsx
+++ NonSealedGenTypes/NonSealedGenTypes/Test.fsx
@@ -6,15 +6,19 @@ type MyOne() =
     inherit Root.One("one")
 
     override this.GetData(id) = 
-        async.Return(sprintf "Data for %s. Id - %i" this.Greeeting id)
+        async.Return(sprintf "Greeting: %s. Id - %i" this.Greeeting id)
 
 let myone = MyOne()
 myone.Greeeting
 myone.GetData(42) |> Async.RunSynchronously
 
-let two = {
-    new Root.Two("tow") with
+let getNew name = {
+    new Root.Two(name) with
         member this.GetData(id) = 
-            async.Return(sprintf "Data for %s. Id - %i" this.Greeeting id)
+            async.Return(sprintf "Special message from Data for %s. Id - %i" this.Greeeting id)
 }
+
+let two = getNew "two"
+two.Greeeting
+two.GetData -1 |> Async.RunSynchronously
 
