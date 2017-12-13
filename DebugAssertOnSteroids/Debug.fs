c49a4717f2d6fed2c3ba663f1af8c41ee2aed585
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2017-12-12 21:35:31 -0800

    WIP

--- DebugAssertOnSteroids/Debug.fs
+++ DebugAssertOnSteroids/Debug.fs
@@ -13,45 +13,45 @@ type Debug =
     [<ConditionalAttribute("DEBUG")>]
     static member Assert([<ReflectedDefinition>] condition: Expr<bool>, [<CallerFilePath>]?filePath: string, [<CallerLineNumber>]?line: int) : unit =
         
-        let rec prettyPrintExpr e = 
+        let rec prettyPrinty e = 
             match e with 
             | SpecificCall <@ (=) @> (None, _, [ lhs;  rhs ]) ->
-                sprintf "%s = %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s = %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | SpecificCall <@ (<) @> (None, _, [ lhs;  rhs ]) ->
-                sprintf "%s < %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s < %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | SpecificCall <@ (>) @> (None, _, [ lhs;  rhs ]) ->
-                sprintf "%s > %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s > %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | SpecificCall <@ (<>) @> (None, _, [ lhs;  rhs ]) ->
-                sprintf "%s <> %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s <> %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | SpecificCall <@ (>=) @> (None, _, [ lhs;  rhs ]) ->
-                sprintf "%s >= %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s >= %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | SpecificCall <@ (<=) @> (None, _, [ lhs;  rhs ]) ->
-                sprintf "%s <= %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s <= %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | SpecificCall <@ not @> (None, _, [ x ]) ->
-                sprintf "not %s" (prettyPrintExpr x) 
+                sprintf "not %s" (prettyPrinty x) 
             | PropertyGet( Some( self), prop, args) -> 
-                sprintf "%s.%s" (prettyPrintExpr self) prop.Name
+                sprintf "%s.%s" (prettyPrinty self) prop.Name
             | Call(None, method, xs) ->
-                let args = xs |> List.map prettyPrintExpr |> String.concat","
+                let args = xs |> List.map prettyPrinty |> String.concat","
                 let invocation = 
                     if Char.IsLower(method.Name.[0]) && xs.Length = 1
-                    then sprintf "%s %s" method.Name (prettyPrintExpr xs.Head)
+                    then sprintf "%s %s" method.Name (prettyPrinty xs.Head)
                     else sprintf "%s(%s)" method.Name args
                 sprintf "%s.%s" method.DeclaringType.Name invocation
             | OrElse (lhs, rhs) ->
-                sprintf "%s || %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s || %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | AndAlso (lhs, rhs) ->
-                sprintf "%s && %s" (prettyPrintExpr lhs) (prettyPrintExpr rhs)
+                sprintf "%s && %s" (prettyPrinty lhs) (prettyPrinty rhs)
             | ValueWithName(_, _, name) -> 
                 name
             | Value(x, _) -> 
                 sprintf "%A" x 
             | _ -> "" 
 
-        let evalCondition =  LeafExpressionConverter.EvaluateQuotation(condition) :?> _
+        let evalCondition = LeafExpressionConverter.EvaluateQuotation(condition) :?> _
         if not evalCondition
         then 
-            let expr = prettyPrintExpr condition
+            let expr = prettyPrinty condition
             let location = sprintf "\nat %s:line %i" filePath.Value line.Value
             let message = sprintf "Assertion (%s) failed%s" expr location
             System.Diagnostics.Debug.Assert( evalCondition, message)
